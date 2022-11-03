c42f708975ee936a846d8efc74b9f6667925f991
Elsa-B <elsaz2.barrero@gmail.com>
2022-11-03 10:26:29 +0100

    Mise en place api

--- front/js/product.js
+++ front/js/product.js
@@ -2,8 +2,22 @@
 
 /*Mise en place d'une nouvelle URL.
 Utilisation de window pour récupérer la page courante*/
-const paramPage = new URLSearchParams(window.location.href);
+const newUrl = window.location.search;
+//Création d'une nouvelle URL
+const urlParams = new URLSearchParams(newUrl);
 //Création de l'id de la nouvelle URL
-const id = paramPage.get("id")
+const id = urlParams.get('id');
 
- console.log(paramPage)
+
+//Je fais appel à fetch pour l'URL de la page produit
+fetch ("http://localhost:3000/api/products/",urlParams.get)
+//Première promesse, qui récupére la réponse en json
+    .then ((res) => {
+        if(res.ok){
+            return res.json();
+        }
+    })
+    //Message d'erreur si le serveur ne répond pas
+    .catch(function(error){
+        alert("Le serveur ne répond pas !!!");
+    })
