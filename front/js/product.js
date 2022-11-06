a0f0e93fa998018a086ba40fba1e766f51dee164
Elsa-B <elsaz2.barrero@gmail.com>
2022-11-06 10:28:18 +0100

    Affichage du produit

--- front/js/product.js
+++ front/js/product.js
@@ -9,11 +9,19 @@ const urlParams = new URLSearchParams(newUrl);
 const id = urlParams.get('id');
 const urlProduct = `http://localhost:3000/api/products/${id}`;
 
-/*Variables pour l'affichage de mes produits*/
-const imageProduct = document.getElementsByClassName("item__img");
+
+/*Variables pour l'affichage de mes produits
+Variable de l'image*/
+const image = document.querySelector(".item__img");
+const imageProduct = document.createElement("img");
+//Variable du nom du produit
 const titleProduct = document.getElementById("title");
+//Variable du prix
 const pricePoduct = document.getElementById("price");
+//Variable de la description
 const descriptionProduct = document.getElementById("description");
+//Variable de l'option
+const color = document.getElementById("colors");
 
 
 //Je fais appel à fetch pour l'URL de la page produit
@@ -32,8 +40,23 @@ fetch (urlProduct)
     })
 
     function product (article){
-        imageProduct.innerHTML = article.imageUrl, article.altTxt;
-        titleProduct.innerHTML = article.name;
-        pricePoduct.innerHTML = article.price;
-        descriptionProduct.innerHTML= article.description;
+    //Insertion des images
+    imageProduct.src = article.imageUrl;
+    imageProduct.alt = article.altTxt;
+    image.appendChild(imageProduct);
+    //Insertion du nom
+    titleProduct.innerText = article.name;
+    //Insertion du prix
+    pricePoduct.innerText = article.price;
+    //Insertion de la description
+    descriptionProduct.innerText= article.description;
+    /*Insertion de l'option
+    Utilisation d'une boucle*/
+    for(let i=0; i<article.colors.length; i++){
+        const option = document.createElement("option");
+        option.innerText = article.colors[i];
+        color.appendChild(option);
+        }   
     }
+
+/*Ajout des produits dans le panier*/
