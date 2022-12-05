bd76327c2258137096c1fe741e8d145f776e6c6e
Elsa-B <elsaz2.barrero@gmail.com>
2022-12-05 11:19:55 +0100

    Finalisation confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,4 @@
-//Récupération de l'URL //Récupération du numéro de commande
+//Récupération de l'URL. Ensuite, récupération du numéro de commande et affichage du numéro de commande
 let newId = new URL(window.location.href).searchParams.get("orderId");
-//Endroit où doit être affiché le numéro de commande
 document.getElementById("orderId").innerHTML = `${newId}`;
 localStorage.clear();
