a6119d4c8613f7287d867e60ea742562a423cd77
Elsa-B <elsaz2.barrero@gmail.com>
2022-12-15 13:00:37 +0100

    Fin confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,5 @@
-//Récupération de l'URL. Ensuite, récupération du numéro de commande et affichage du numéro de commande. Suppression du lS
+/*Récupération de l'URL. Ensuite, récupération du numéro de commande et affichage du numéro de commande.
+Suppression du lS*/
 let newId = new URL(window.location.href).searchParams.get("orderId");
 document.getElementById("orderId").innerHTML = newId;
 localStorage.clear();
