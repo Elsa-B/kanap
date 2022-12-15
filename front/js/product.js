9a95e9b1e150279e5a6220cba1b88ef8870da8ce
Elsa-B <elsaz2.barrero@gmail.com>
2022-12-15 12:45:14 +0100

    Fin products

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,6 @@
 //             Lien entre la page d'accueil et les produits
-/*Mise en place d'une nouvelle URL.
-Utilisation de window pour récupérer la page courante. Ensuite, création d'une nouvelle URL. Pour finir, création de l'id de la nouvelle URL*/
+/*Mise en place d'une nouvelle URL. Utilisation de window pour récupérer la page courante.
+On accède aux arguments. Ensuite, création d'une nouvelle URL avec l'id.*/
 const newUrl = window.location.search;
 const urlParams = new URLSearchParams(newUrl);
 const id = urlParams.get('id');
@@ -23,7 +23,8 @@ const choiceQuantite = document.getElementById("quantity");
 //Variable du bouton "Ajouter au panier"
 const check = document.getElementById("addToCart");
 
-/*Je fais appel à fetch pour l'URL de la page produit. Première promesse, qui récupére la réponse en json. Deuxième promesse pour l'affichage des produits.
+/*Je fais appel à fetch pour exécuter l'URL de la page produit.
+Première promesse, qui récupére la réponse en json. Deuxième promesse pour l'affichage des produits.
 Message d'erreur si le serveur ne répond pas*/
 fetch (urlProduct)
     .then ((res) => {
@@ -54,20 +55,21 @@ function product (article){
         color.appendChild(option);
     };  
 };
-//Conversion des données du format JSON au format Javascript
+//Variable de conversion des données du format JSON au format Javascript et récupération des données
 let productInBasket = JSON.parse(localStorage.getItem("basket"));
-//Variable de confirmation d'ajout du produit
+//Fonction de confirmation d'ajout du produit
 const confirm = () => {
     alert ("Le produit a été ajouté au panier");
 };
-/*                 Ajout des produits dans le panier
-Récupération de la sélection de l'utilisateur et envoie dans le panier.
-Ecoute de la variable "check" au moment du clic de l'utilisateur. Demande de non-exécution de l'évenement. Récupération de la sélection utilisateur avec l'ID, la quantité et la couleur.*/
-/*Utilisation des conditions. Si on ajoute un produit au panier
-Si, la couleur est true entre les "" et la quantité est >0 et <=100, alors, si il y a des pdts dans le panier, utilisation de find qui renvoie la première valeur d'un élément dans un array.
-Si, le pdt existe déjà dans l'array, alors calcul pour ajouter les quantités dans l'array. Et envoie pour mise à jour dans le LS, message confirmation.
-Sinon, s'il n'y en a pas ajout d'une ligne+message confirmation. S'il n'y a aucun produit dans le localStorage, création d'un tableau, envoie dans le LS et message confirmation.
-Sinon, message d'alerte*/
+/*           Récupération de la sélection de l'utilisateur et envoie dans le panier.
+Ecoute de la variable "check" au moment du clic de l'utilisateur. Demande de non-exécution de l'évenement.
+Objet de récupération de la sélection utilisateur avec l'ID, la quantité et la couleur.
+Utilisation des conditions. Si, la couleur est true entre les "" et la quantité est >0 et <=100, alors,si 
+il y a des pdts dans le panier, utilisation de find qui renvoie la première valeur d'un élément dans un array.
+Si, le pdt existe déjà dans l'array, alors calcul pour ajouter les quantités dans l'array.
+Et envoie pour mise à jour dans le LS, message confirmation.
+Sinon, s'il n'y en a pas ajout des produits dans le tableau+message confirmation. S'il n'y a aucun produit
+dans le localStorage, création d'un tableau, envoie dans le LS et message confirmation. Sinon, message d'alerte*/
 function sendInBasket(){
     check.addEventListener("click",(e)=>{
         e.preventDefault();
@@ -77,11 +79,11 @@ function sendInBasket(){
             couleurStorage:color.value,
         };
         console.log(choiceProduct);
-        //Variables de stockage
+        //Fonction de stockage
         let eltLocalStorage= () =>{
             //Ajout des éléments dans le tableau avec push
             productInBasket.push(choiceProduct);
-            //Conversion des données du format javaScript au format JSON
+            //Stockage des données et conversion de celles-ci, du format javaScript au format JSON
             localStorage.setItem("basket", JSON.stringify(productInBasket)); 
         }
         if(choiceProduct.couleurStorage !== "" && choiceProduct.quantiteStorage > 0 && choiceProduct.quantiteStorage <= 100){
